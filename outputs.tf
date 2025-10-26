5a68f83a756b57ee044596b824002daaae8e6f58
Thiarinho <thiernomane932@gmail.com>
2025-10-26 21:07:35 +0000

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_public_ip" {
-  description = "Adresse IP publique de l’instance EC2"
-  value       = aws_instance.web.public_ip
-}
-
-output "rds_endpoint" {
-  description = "Endpoint de la base de données RDS"
-  value       = aws_db_instance.db.endpoint
-}
